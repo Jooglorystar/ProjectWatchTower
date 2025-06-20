00c4aecb796276b3c6d5b389f4c94c5e13f5d41f
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2025-06-20 23:26:34 +0900

    Refactor: Extract UIButton and UITextMeshPro into separate files

---
+++ Assets/Scripts/UI/Buttons/UIButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class UIButton : MonoBehaviour
+{
+    protected Image image;
+    protected Button button;
+
+    protected virtual void Awake()
+    {
+        image = GetComponent<Image>();
+        button = GetComponent<Button>();
+    }
+}
--- Assets/Scripts/UI/Buttons/UIRetryButton.cs
+++ Assets/Scripts/UI/Buttons/UIRetryButton.cs
@@ -1,18 +1,3 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-public abstract class UIButton : MonoBehaviour
-{
-    protected Image image;
-    protected Button button;
-
-    protected virtual void Awake()
-    {
-        image = GetComponent<Image>();
-        button = GetComponent<Button>();
-    }
-}
-
 public class UIRetryButton : UIButton
 {
     protected override void Awake()
